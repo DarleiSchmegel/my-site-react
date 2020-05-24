7c14fe89720ef4878b3736791cf41545788af515
Darlei Schmegel <darleischmegel94@gmail.com>
2020-05-24 14:55:31 -0300

    teste 4

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import { HashRouter, Route, Link } from "react-router-dom";
 
 import Home from './pages/Home';
 import About from './pages/About';
+
+import Routes from './routes';
 class App extends Component {
   render() {
     return (
+      <>
+      
       <HashRouter basename="/">
         <div>
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
+            
+            <li><Link to="/main">Main</Link></li>
           </ul>
 
           <hr />
-  
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
+          
+          <Routes />
+          
         </div>
       </HashRouter>
+      </>
     );
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Switch , Route } from 'react-router-dom';
+import { BrowserRouter, Switch , Route, HashRouter } from 'react-router-dom';
 
 import Main from './pages/Main';
-
+import Home from './pages/Home';
+import About from './pages/About';
 
 export default function Routes() {
     return(
+        <HashRouter basename="/">
        
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Main} />
-               
-            </Switch> 
-        </BrowserRouter>
+                
+                <Route path="/" exact component={Home} />
+                <Route path="/main" component={Main} />
+                <Route path="/about" component={About} />
+          
+        </HashRouter>
     );
 }
