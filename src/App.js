792ed031394a42bec4e3f489ba3c42f73a1ce3a9
Darlei Schmegel <darleischmegel94@gmail.com>
2020-05-24 12:14:06 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           test for git pages. My second test
         </p>
-        <i class="fa fa-external-link-square" aria-hidden="true"></i>
       </header>
     </div>
   );
